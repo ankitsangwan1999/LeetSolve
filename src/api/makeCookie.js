b243c1d1008383b5f3a548b7b960f7ba4cccd36a
ankitsangwan1955 <91696583+ankitsangwan1955@users.noreply.github.com>
2021-10-01 00:47:06 +0530

    Update makeCookie.js

--- src/api/makeCookie.js
+++ src/api/makeCookie.js
@@ -1,6 +1,6 @@
 export default (value) => {
-	const currMilliSeconds = new Date().getTime();
-	const endSeconds = currMilliSeconds / 1000 + 84600;
+	const currMilliSeconds = new Date().getTime(); // Give milliseconds since EPOCH
+	const endSeconds = currMilliSeconds / 1000 + 84600; // Cookie will expire in 24 hours
 	return {
 		url: "https://leetsolve.matrix.com",
 		name: "MY_TASTY_COOKIE",

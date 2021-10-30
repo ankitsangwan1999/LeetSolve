6f889e6d1bd7e46c935ee1c6e5595667d87259cf
Kunal Katiyar <kunalkatiyar50@gmail.com>
2021-10-31 01:37:18 +0530

    Pagination added to other tables as well

--- src/components/AttemptedQuestionsTable.js
+++ src/components/AttemptedQuestionsTable.js
@@ -12,104 +12,231 @@ import {
 } from "../styles/table";
 import { Scrollable } from "../styles/scrollbar";
 import { ShuffleButton } from "../styles/shufflestyle";
+import { useTable, usePagination } from "react-table";
+
+function PaginationTable({ columns, data, onShuffle_var }) {
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		prepareRow,
+		page,
+		canPreviousPage,
+		canNextPage,
+		pageOptions,
+		pageCount,
+		gotoPage,
+		nextPage,
+		previousPage,
+		setPageSize,
+		state: { pageIndex, pageSize },
+	} = useTable(
+		{
+			columns,
+			data,
+			initialState: { pageIndex: 0 },
+		},
+		usePagination
+	);
+
+	return (
+		<>
+			<div className="pagination" style={{
+				 position: "absolute",
+				 right: "0",
+				 padding: "0px 27px 0px 0px"
+			}}>
+				<button style={{background: "#000000", color: "#FFFF00"}}
+				onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+					{"<<"}
+				</button>{" "}
+				<button style={{background: "#000000", color: "#FFFF00"}}
+					onClick={() => previousPage()}
+					disabled={!canPreviousPage}
+				>
+					{"<"}
+				</button>{" "}
+				<button style={{background: "#000000", color: "#FFFF00"}}
+				onClick={() => nextPage()} disabled={!canNextPage}>
+					{">"}
+				</button>{" "}
+				<button style={{background: "#000000", color: "#FFFF00"}}
+					onClick={() => gotoPage(pageCount - 1)}
+					disabled={!canNextPage}
+				>
+					{">>"}
+				</button>{" "}
+				<span style={{fontFamily: "Cool Font"}}>
+					Page{" "}
+					<strong>
+						{pageIndex + 1} of {pageOptions.length}
+					</strong>{" "}
+					| Go to page:{" "}
+					<input
+						type="number"
+						defaultValue={pageIndex + 1}
+						onChange={(e) => {
+							const page = e.target.value
+								? Number(e.target.value) - 1
+								: 0;
+							gotoPage(page);
+						}}
+						style={{ width: "50px",
+								background: "#000000",
+								color: "#FFFF00", }}
+					/>
+				</span>{" "}
+				<span>
+				<select
+					style={{ background: "#000000",
+							color: "#ffffff",
+							fontFamily: "Cool Font", }}
+					value={pageSize}
+					onChange={(e) => {
+						setPageSize(Number(e.target.value));
+					}}
+				>
+					{[10, 20, 30, 40, 50].map((pageSize) => (
+						<option key={pageSize} value={pageSize}>
+							Show {pageSize}
+						</option>
+					))}
+				</select></span>
+			</div>
+			<Table {...getTableProps()} style={{
+				padding: "25px 0px 0px 0px",
+			}}>
+			<THead>
+				{headerGroups.map((headerGroup) => (
+					<Tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map((column) => (
+							<Th {...column.getHeaderProps()}>
+									{(() => {
+									switch (column.Header) {
+										case "Title":   
+											console.log(column.Header);	
+											return (<ShuffleButton>
+												<img
+													src="src/static/images/random.png"
+													style={{
+														float: "left",
+														width: "2rem",
+													}}
+													onClick={onShuffle_var}
+													// title="Pick one Random Problem"
+												/>
+												<span>Pick one Random Question</span>
+											</ShuffleButton>)
+									}
+								})()}
+								{column.render("Header")}
+							</Th>
+						))}
+					</Tr>
+				))}
+			</THead>
+			<div></div>
+			{/* <Table {...getTableProps()}> */}
+			{/* <Scrollable maxHeight="50vh"> */}
+				<TBody {...getTableBodyProps()}>
+					{page.map((row, i) => {
+						prepareRow(row);
+						return (
+							<Tr {...row.getRowProps()}>
+								{row.cells.map((cell) => {
+									return (
+										<Td {...cell.getCellProps()}>
+											{cell.render("Cell")}
+										</Td>
+									);
+								})}
+							</Tr>
+						);
+					})}
+				</TBody>
+			{/* </Scrollable> */}
+			</Table>
+		</>
+	);
+}
+
 const AttemptedQuestionsTable = ({ data, onShuffle }) => {
 	console.log("DATA:", data);
+	console.log(typeof shuffle_image);
+
+	//extracting useful information
+	const useful_data = [];
+	data["stat_status_pairs"].map((que, index) => {
+		if (
+				!(
+					que["status"] === "ac" ||
+					que["status"] === "notac"
+				)
+			) {
+			//  return null if not attempted i.e. neither 'ac' nor 'notac'
+			return null;
+		}
+		useful_data.push({
+			href: "https://leetcode.com/problems/"+que["stat"]["question__title_slug"],
+			ques_id: que["stat"]["frontend_question_id"],
+			ques_title: que["stat"]["question__title"],
+			level:
+				que["difficulty"]["level"] == 1
+					? "Easy"
+					: que["difficulty"]["level"] == 2
+					? "Medium"
+					: "Hard",
+			status:
+				que["status"] === "ac"
+					? "AC"
+					: "Not-AC",
+			paid: que["paid_only"] === true ? "Premium" : "Free",
+		});
+	});
+	console.log(useful_data);
+
+	//defining column names and attributes
+	const columns = React.useMemo(
+		() => [
+			{
+				width: 9,
+				minWidth: 9,
+				Header: "ID",
+				accessor: "ques_id",
+			},
+			{
+				width: 55,
+				Header: "Title",
+				minWidth: 55,
+				id: "link",
+				accessor: d => d.href,
+				Cell: ({ row }) => <a href={row.original.href} target="_blank">{row.original.ques_title}</a>
+			},
+			{
+				width: 12,
+				minWidth: 12,
+				Header: "Level",
+				accessor: "level",
+			},
+			{
+				width: 12,
+				minWidth: 12,
+				Header: "Status",
+				accessor: "status",
+			},
+			{
+				width: 12,
+				minWidth: 12,
+				Header: "Paid",
+				accessor: "paid",
+			},
+		],
+		[]
+	);
+
 	return (
 		<TableContainer>
-			<Table>
-				<colgroup>
-					<col span="1" style={{ width: "9%" }} />
-					<col span="1" style={{ width: "55%" }} />
-					<col span="1" style={{ width: "12%" }} />
-					<col span="1" style={{ width: "12%" }} />
-					<col span="1" style={{ width: "12%" }} />
-				</colgroup>
-				<THead>
-					<Tr>
-						<Th>ID</Th>
-						<Th style={{ color: "00f2ff" }}>
-							<ShuffleButton>
-								<img
-									src="src/static/images/random.png"
-									style={{
-										float: "left",
-										width: "2rem",
-									}}
-									onClick={onShuffle}
-									// title="Pick one Random Problem"
-								/>
-								<span>Pick one Random Question</span>
-							</ShuffleButton>
-							Title
-						</Th>
-						<Th style={{ color: "pink" }}>Level</Th>
-						<Th style={{ color: "00fff5" }}>Status</Th>
-						<Th>Paid</Th>
-					</Tr>
-				</THead>
-			</Table>
-			<div></div>
-			<Scrollable maxHeight="68vh">
-				<Table>
-					<colgroup>
-						<col span="1" style={{ width: "9%" }} />
-						<col span="1" style={{ width: "55%" }} />
-						<col span="1" style={{ width: "12%" }} />
-						<col span="1" style={{ width: "12%" }} />
-						<col span="1" style={{ width: "12%" }} />
-					</colgroup>
-					<TBody>
-						{data["stat_status_pairs"].map((que, index) => {
-							if (
-								!(
-									que["status"] === "ac" ||
-									que["status"] === "notac"
-								)
-							) {
-								//  return null if not attempted i.e. neither 'ac' nor 'notac'
-								return null;
-							}
-
-							// if attempted then return as a row for the table
-							return (
-								<Tr key={index}>
-									<Td>
-										{que["stat"]["frontend_question_id"]}
-									</Td>
-									<Td>
-										<a
-											href={`https://leetcode.com/problems/${que["stat"]["question__title_slug"]}`}
-											target="_blank"
-											rel="noreferrer"
-										>
-											{que["stat"]["question__title"]}
-										</a>
-									</Td>
-									<Td>
-										{que["difficulty"]["level"] == 1
-											? "Easy"
-											: que["difficulty"]["level"] == 2
-											? "Medium"
-											: "Hard"}
-									</Td>
-									<Td>
-										{que["status"] === "ac"
-											? "AC"
-											: que["status"] === "notac"
-											? "Not-AC"
-											: "Not-Attempted"}
-									</Td>
-									<Td>
-										{que["paid_only"] === true
-											? "Premium"
-											: "Free"}
-									</Td>
-								</Tr>
-							);
-						})}
-					</TBody>
-				</Table>
-			</Scrollable>
+			<PaginationTable columns={columns} data={useful_data} />
 		</TableContainer>
 	);
 };
